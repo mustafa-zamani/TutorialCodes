1bb78239b0273e27195d8e875f904f0825d34eb6
Mustafa Zamani <zamani.mustafa91@gmail.com>
2022-12-08 20:18:10 +0100

    login connection added

--- cmsProjectPHP/MyPractice/login.php
+++ cmsProjectPHP/MyPractice/login.php
@@ -1,6 +1,25 @@
 <?php
 
-if(isset($_POST['submit'])){
-    echo "submit";
+if(isset($_POST['submit']))
+{
+    $userName = $_POST['userName'];
+    $password = $_POST['password'];
+    
+    $connection = mysqli_connect('localhost', 'root','', 'loginapp');
+    if($connection)
+    {
+        echo "Connection Stablished\n";
+    }else
+    {
+        echo "Connection Faild";
+    }
+
+    if($userName && $password)
+    {
+        echo "Your Username is: $userName and Your password is: $password\n";
+    }else
+    {
+        echo "username or password is blank";
+    }
 }
 ?>
